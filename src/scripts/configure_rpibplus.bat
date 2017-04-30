8b0cb940f82557a218ff85ca86e04bf9ab41a985
David Banks <dave@hoglet.com>
2017-04-30 10:56:02 +0100

    Deleted unused build files
    Change-Id: I3b2a8a327f8509dc2955a2fff038db0266ceaee3

--- src/scripts/configure_rpibplus.bat
+++
@@ -1,1 +0,0 @@
-cmake -G "CodeBlocks - MinGW Makefiles" %*  -DCMAKE_TOOLCHAIN_FILE=../toolchain-arm-none-eabi-rpibplus.cmake ../
--- src/scripts/configure_rpizero.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-
-cmake -G "CodeBlocks - Unix Makefiles" "$*" -DCMAKE_TOOLCHAIN_FILE=../toolchain-arm-none-eabi-rpizero.cmake ../
