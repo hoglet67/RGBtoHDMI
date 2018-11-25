fd90602515ded6decc581bf4350884afb2381196
David Banks <dave@hoglet.com>
2018-11-25 13:51:33 +0000

    Pi Firmware: Tidied up cpld_atom
    Change-Id: I219b27c7c2f9bdebee2ca780ac5726d84172a4cc

--- src/cpld_atom.c
+++ src/cpld_atom.c
@@ -25,26 +25,22 @@ typedef struct {
    int clock;         // cpld clock (in Hz)
    int line_len;      // number of clocks per horizontal line
    int n_lines;       // number of horizontal lines per frame (two fields)
-   int divider;       // cpld divider: 0=divide-by-6; 1 = divide-by-8
 } config_t;
 
 // Current calibration state for mode 0..6
 static config_t default_config;
 
 // Current configuration
-static config_t *config;
+static config_t *config = &default_config;
 
 // OSD message buffer
 static char message[80];
 
-// Per-Offset calibration metrics (i.e. errors) for mode 0..6
-static int raw_metrics_default[RANGE][NUM_OFFSETS];
-
-// Aggregate calibration metrics (i.e. errors summed across all offsets) for mode 0..6
-static int sum_metrics_default[RANGE]; // Last two not used
+// Aggregate calibration metrics (i.e. errors summed across all channels)
+static int sum_metrics[RANGE];
 
 // Error count for final calibration values for mode 0..6
-static int errors_default;
+static int errors;
 
 // The CPLD version number
 static int cpld_version;
@@ -66,16 +62,15 @@ enum {
    CLOCK,
    LINE_LEN,
    N_LINES,
-   DIVIDER,
 };
 
-static param_t default_sampling_params[] = {
+static param_t sampling_params[] = {
    {      OFFSET,      "Offset", 0,  15 },
    {          -1,          NULL, 0,   0 }
 };
 
 
-static param_t default_geometry_params[] = {
+static param_t geometry_params[] = {
    {  H_OFFSET,        "H offset",         0,        59 },
    {  V_OFFSET,        "V offset",         0,        39 },
    {   H_WIDTH,         "H width",         1,       100 },
@@ -85,7 +80,6 @@ static param_t default_geometry_params[] = {
    {     CLOCK,      "Clock freq",  75000000, 100000000 },
    {  LINE_LEN,     "Line length",      1000,      9999 },
    {   N_LINES, "Lines per frame",       500,       699 },
-   {   DIVIDER,         "Divider",         0,         1 },
    {        -1,              NULL,         0,         0 }
 };
 
@@ -137,25 +131,20 @@ static void log_sp(config_t *config) {
 
 static void cpld_init(int version) {
    cpld_version = version;
-   default_config.h_offset  =       19;
-   default_config.v_offset  =       11;
-   default_config.h_width   =       76;
-   default_config.v_height  =      240;
-   default_config.fb_width  =      608;
-   default_config.fb_height =      480;
-   default_config.clock     = 57272720;
-   default_config.line_len  =     3648;
-   default_config.n_lines   =      524;
-   default_config.divider   =        0;
-   default_config.sp_offset =        2;
-   config = &default_config;
+   config->h_offset  =       19;
+   config->v_offset  =       11;
+   config->h_width   =       76;
+   config->v_height  =      240;
+   config->fb_width  =      608;
+   config->fb_height =      480;
+   config->clock     = 57272720;
+   config->line_len  =     3648;
+   config->n_lines   =      524;
+   config->sp_offset =        2;
    for (int i = 0; i < RANGE; i++) {
-      sum_metrics_default[i] = -1;
-      for (int j = 0; j < NUM_OFFSETS; j++) {
-         raw_metrics_default[i][j] = -1;
-      }
+      sum_metrics[i] = -1;
    }
-   errors_default = -1;
+   errors = -1;
 }
 
 static int cpld_get_version() {
@@ -173,18 +162,9 @@ static void cpld_calibrate(capture_info_t *capinfo, int elk) {
    int win_metric;     // this is a windowed value (over three sample offsets)
    int min_win_metric;
    int *rgb_metric;
-
-   int range;          // 0..5 in Modes 0..6, 0..7 in Mode 7
-   int *sum_metrics;
-   int *errors;
-
-   int (*raw_metrics)[RANGE][NUM_OFFSETS];
+   int range = RANGE;  // 0..15 for the Atom
 
    log_info("Calibrating...");
-   range       = RANGE;
-   raw_metrics = &raw_metrics_default;
-   sum_metrics = &sum_metrics_default[0];
-   errors      = &errors_default;
 
    // Measure the error metrics at all possible offset values
    min_metric = INT_MAX;
@@ -196,16 +176,9 @@ static void cpld_calibrate(capture_info_t *capinfo, int elk) {
    for (int value = 0; value < range; value++) {
       config->sp_offset = value;
       write_config(config);
-      rgb_metric = diff_N_frames_by_sample(capinfo, NUM_CAL_FRAMES, 0, elk);
-      metric = 0;
-      printf("INFO: value = %d: metrics = ", value);
-      for (int i = 0; i < NUM_OFFSETS; i++) {
-         int offset_metric = sum_channels(rgb_metric + i * NUM_CHANNELS);
-         (*raw_metrics)[value][i] = offset_metric;
-         metric += offset_metric;
-         printf("%7d", offset_metric);
-      }
-      printf("%8d\r\n", metric);
+      rgb_metric = diff_N_frames(capinfo, NUM_CAL_FRAMES, 0, elk);
+      metric = sum_channels(rgb_metric);
+      log_info("INFO: value = %d: metric = ", metric);
       sum_metrics[value] = metric;
       osd_sp(config, 1, metric);
       if (metric < min_metric) {
@@ -235,18 +208,14 @@ static void cpld_calibrate(capture_info_t *capinfo, int elk) {
    // Perform a final test of errors
    log_info("Performing final test");
    rgb_metric = diff_N_frames(capinfo, NUM_CAL_FRAMES, 0, elk);
-   *errors = sum_channels(rgb_metric);
-   osd_sp(config, 1, *errors);
+   errors = sum_channels(rgb_metric);
+   osd_sp(config, 1, errors);
    log_sp(config);
-   log_info("Calibration complete, errors = %d", *errors);
+   log_info("Calibration complete, errors = %d", errors);
 }
 
 static void cpld_set_mode(int mode) {
-   config = &default_config;
    write_config(config);
-   // The "mode7" pin on the CPLD switches between 6 clocks per
-   // pixel (modes 0..6) and 8 clocks per pixel (mode 7)
-   RPI_SetGpioValue(MODE7_PIN, config->divider);
 }
 
 static void cpld_get_fb_params(capture_info_t *capinfo) {
@@ -265,11 +234,11 @@ static void cpld_get_clk_params(clk_info_t *clkinfo) {
 }
 
 static param_t *cpld_get_sampling_params() {
-   return default_sampling_params;
+   return sampling_params;
 }
 
 static param_t *cpld_get_geometry_params() {
-   return default_geometry_params;
+   return geometry_params;
 }
 
 static int cpld_get_value(int num) {
@@ -294,8 +263,6 @@ static int cpld_get_value(int num) {
       return config->line_len;
    case N_LINES:
       return config->n_lines;
-   case DIVIDER:
-      return config->divider;
    }
    return 0;
 }
@@ -332,44 +299,23 @@ static void cpld_set_value(int num, int value) {
    case N_LINES:
       config->n_lines = value;
       break;
-   case DIVIDER:
-      config->divider = value;
-      break;
    }
    write_config(config);
 }
 
 static void cpld_show_cal_summary(int line) {
-   osd_sp(config, line, errors_default);
+   osd_sp(config, line, errors);
 }
 
 static void cpld_show_cal_details(int line) {
-   int *sum_metrics = sum_metrics_default;
    int range = (*sum_metrics < 0) ? 0 : RANGE;
    if (range == 0) {
       sprintf(message, "No calibration data for this mode");
       osd_set(line, 0, message);
    } else {
-      for (int value = 0; value < range; value++) {
-         sprintf(message, "Offset %d: Errors = %6d", value, sum_metrics[value]);
-         osd_set(line + value, 0, message);
-      }
-   }
-}
-
-static void cpld_show_cal_raw(int line) {
-   int (*raw_metrics)[RANGE][NUM_OFFSETS] = &raw_metrics_default;
-   int range = ((*raw_metrics)[0][0] < 0) ? 0 : RANGE;
-   if (range == 0) {
-      sprintf(message, "No calibration data for this mode");
-      osd_set(line, 0, message);
-   } else {
-      for (int value = 0; value < range; value++) {
-         char *mp = message;
-         mp += sprintf(mp, "%d:", value);
-         for (int i = 0; i < NUM_OFFSETS; i++) {
-            mp += sprintf(mp, "%6d", (*raw_metrics)[value][i]);
-         }
+      int num = range >> 1;
+      for (int value = 0; value < num; value++) {
+         sprintf(message, "Offset %d: %6d; Offset %2d: %6d", value, sum_metrics[value], value + num, sum_metrics[value + num]);
          osd_set(line + value, 0, message);
       }
    }
@@ -388,6 +334,5 @@ cpld_t cpld_atom = {
    .get_value = cpld_get_value,
    .set_value = cpld_set_value,
    .show_cal_summary = cpld_show_cal_summary,
-   .show_cal_details = cpld_show_cal_details,
-   .show_cal_raw = cpld_show_cal_raw
+   .show_cal_details = cpld_show_cal_details
 };
