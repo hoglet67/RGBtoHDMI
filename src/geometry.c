9f44b7330fb159d48290f7dc09638aba11015e90
IanSB <isb@clara.co.uk>
2021-08-19 11:08:21 +0100

    Increase max width of screen area used in 1080p

--- src/geometry.c
+++ src/geometry.c
@@ -410,7 +410,7 @@ void geometry_get_fb_params(capture_info_t *capinfo) {
             if (apparent_width > 1440 && geometry->max_h_width <= 720 && geometry->max_h_width > 400 && geometry->min_h_width > 533 && (get_scaling() < SCALING_FILL43_SOFT || geometry->fb_sizex2 != 0)) {
                     left =  (apparent_width - 1440) / 2;
                     right = left;
-            } else if (apparent_width > 1600) {
+            } else if (apparent_width > 1600 && geometry->min_h_width > 533) {
                     left =  (apparent_width - 1600) / 2;
                     right = left;
             }
@@ -582,7 +582,7 @@ void geometry_get_fb_params(capture_info_t *capinfo) {
 
     if (scaling == GSCALING_INTEGER && v_size43 == v_size && h_size > h_size43) {
         //if ((geometry_max_h_width >= 512 && geometry_max_h_width <= 800) || (geometry_max_h_width > 360 && geometry_max_h_width <= 400)) {
-            h_size43 = (h_size43 * 800) / 720;           //adjust 4:3 ratio on widescreen resolutions to account for up to 800 pixel wide integer sample capture
+            h_size43 = (h_size43 * 912) / 720;           //adjust 4:3 ratio on widescreen resolutions to account for up to 900 pixel wide integer sample capture
             if (h_size43 > h_size) {
                 h_size43 = h_size;
             }
