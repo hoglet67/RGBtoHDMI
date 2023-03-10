808adb7e88a65007ba077993012d484421b3be6d
IanSB <isb@clara.co.uk>
2023-03-10 14:26:07 +0000

    Update GPU/ARM switching

--- src/scripts/Use_ARM_Capture.bat
+++ src/scripts/Use_ARM_Capture.bat
@@ -1,2 +1,5 @@
+@echo Switching to ARM capture
+@echo Warning: GPU capture is preferred
+@pause
 ren config.txt configG.txt
 ren configA.txt config.txt
--- src/scripts/Use_GPU_Capture.bat
+++ src/scripts/Use_GPU_Capture.bat
@@ -1,2 +1,4 @@
+@echo Switching to GPU capture
+@pause
 ren config.txt configA.txt
 ren configG.txt config.txt
